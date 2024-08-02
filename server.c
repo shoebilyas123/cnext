9ce1bf88cc563552fe79fa37eeaf975b0876d671
shoebilyas123 <shoebilyas123@gmail.com>
2024-08-02 21:55:35 +0530

    check if the file exists

--- server.c
+++ server.c
@@ -9,8 +9,39 @@
 #include <netdb.h>
 #include <unistd.h>
 
-#define SERVER_PORT "8080"
+#define SERVER_PORT "8081"
 #define BACKLOGS 20
+
+#define MAX_URL_LEN 2048
+#define MAX_REQ_SIZE 4096
+
+struct req_line {
+    char* method;
+    char* url;
+};
+
+
+int get_req_line(struct req_line* req_line_res, char *buffer) {
+    req_line_res->method = strtok(buffer, " ");
+    req_line_res->url = strtok(NULL, " ");
+    return 0;
+}
+
+int getfile_content(char* path, char* fname, char* buffer) {
+    // F_OK: mode parameter to check the existence of a path
+    char* f_path = (char *)malloc(1+strlen(path)+strlen(fname));
+    strcpy(f_path, path);
+    strcat(f_path, fname);
+    strcat(f_path, ".html");
+
+    if(access(f_path, F_OK) != 0) {
+        return errno;
+    }
+
+
+
+    return 0;
+}
 
 int main() {
     // prepare our address info to create socket file descriptor
@@ -22,6 +53,7 @@ int main() {
     int remote_fd;
     struct sockaddr_storage remote_addr;
     socklen_t remote_addr_len = sizeof(remote_addr);
+    struct req_line request_line;
 
     memset(&hints, 0, sizeof(hints));
     hints.ai_family = AF_INET;
@@ -45,7 +77,26 @@ int main() {
     while(1) {
         // Accept will block until there is a connection in the accept queue.
         remote_fd = accept(srvfd, (struct sockaddr *)&remote_addr, &remote_addr_len);
-        printf("Incomgin connection: SOCKET_FD_ID:%d\n", remote_fd);
+        printf("Incoming connection: SOCKET_FD_ID:%d\n", remote_fd);
+
+        char buffer[MAX_REQ_SIZE];
+        recv(remote_fd, buffer, MAX_REQ_SIZE, 0);
+
+        get_req_line(&request_line, buffer);
+
+        printf("METHOD: %s\n", request_line.method);
+        printf("URL: %s\n", request_line.url);
+
+        char pwd[4096];
+        getcwd(pwd, sizeof(pwd));
+        strcat(pwd, "/html");
+
+        char* f_contents;
+
+        if(getfile_content(pwd, request_line.url, f_contents) != 0) {
+            fprintf(stderr,"File does not exist: ERROR %d\n", errno);
+            return errno;
+        }
 
         close(remote_fd);
     }
